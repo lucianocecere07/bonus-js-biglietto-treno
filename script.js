aa91c6d51003e69c5d8a67b566b6e1cf50ccf463
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-10 16:44:24 +0100

    inserimento html e javascript

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
-let km = prompt("Inserire quanti chilometri da percorrere");
-let eta = prompt("Inserire età del passeggero");
+let Totale = document.getElementById("totale");
 
-let prezzo = km * 0.21
-let prezzoDecimali = prezzo.toFixed(2)
+Totale.addEventListener('click', function(){
 
-let scontoMinore = prezzo * 20 / 100
-let scontoOver = prezzo * 40 / 100
+    let km = document.getElementById("input-km").value;
+    let eta = document.getElementById("input-eta").value;
 
-console.log(
-    `
-    ------Dati del passeggero------
-        Chilometri da percorrere: ${km}
-        Età del passeggero: ${eta}
-    ------Prezzo pieno------
-        Prezzo pieno: ${prezzoDecimali} €
-    ------Totale (con sconto dove disponibile)------
-    `
-);
+    let prezzo = km * 0.21
+    let prezzoDecimali = prezzo.toFixed(2)
 
-if(eta < 18){
-    minore = prezzo - scontoMinore
-    minoreDecimali = minore.toFixed(2)
-    console.log ("Totale (sconto minorenne): " + minoreDecimali + " €");
-} else if(eta >= 65){
-    over = prezzo - scontoOver
-    overDecimali = over.toFixed(2)
-    console.log ("Totale (sconto over 65): " + overDecimali + " €");
-} else{
-    console.log ("Totale: " + prezzoDecimali + " €");
-}
+    let scontoMinore = prezzo * 20 / 100
+    let scontoOver = prezzo * 40 / 100
+
+    document.getElementById("totaleSconto").innerHTML = prezzoDecimali;
+
+    console.log(
+        `
+        ------Dati del passeggero------
+            Chilometri del percorso: ${km}
+            Età del passeggero: ${eta}
+        ------Prezzo del biglietto------
+            Prezzo pieno: ${prezzoDecimali} €
+        ------Totale (con sconto dove disponibile)------
+        `
+    );
+
+    if(eta < 18){
+        minore = prezzo - scontoMinore
+        minoreDecimali = minore.toFixed(2)
+        console.log ("Totale (sconto minorenne): " + minoreDecimali + " €");
+    } else if(eta >= 65){
+        over = prezzo - scontoOver
+        overDecimali = over.toFixed(2)
+        console.log ("Totale (sconto over 65): " + overDecimali + " €");
+    } else{
+        console.log ("Totale: " + prezzoDecimali + " €");
+    }
+});
